1981bb0df848e15b3192dec3c47fa9b6b5eb71e1
noremacmada <noremacmada@users.noreply.github.com>
2017-06-06 17:25:18 -0400

    module notation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict;"
-const serverNs = require("./app/server/server.js")
-const server = new serverNs()
-const http = require("http")
-http.createServer(server.requestHandler).listen(3000)
+const mdlServer = require("./app/server/server.js")
+const mdlHttp = require("http")
+
+const server = new mdlServer()
+mdlHttp.createServer(server.requestHandler).listen(3000)
