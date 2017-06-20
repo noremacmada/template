52e28137d18aa2d8d2c5af6425c2a1fa0f0c87ad
noremacmada <noremacmada@users.noreply.github.com>
2017-06-19 20:35:37 -0400

    compiling angular 1.6 apps on the fly

--- app/server/handlers/file.js
+++ app/server/handlers/file.js
@@ -8,134 +8,189 @@ module.exports = class File extends mdlDefault{
         enumerable: false
       }
     )
-    this.getAppDir(){
-      let arrDir = __dirname.split("\\")
-      arrDir.pop()
-      arrDir.pop()
-      return arrDir.join("/")
-    }
+    Object.defineProperty(this, "appDir", {
+        get: () => {
+          let arrDir = __dirname.split("\\")
+          arrDir.pop()
+          arrDir.pop()
+          return arrDir.join("/")
+        },
+        enumerable: false
+      }
+    )
+
     this.pipe = (requestType, params) => {
-      let dirClient = this.getAppDir() + "/client"
+      let dirClient = this.appDir + "/client"
       let filePath = dirClient + "/" + params.relPath
       this.responseWrapper.static(filePath)
     }
-    this.include(requestType, params)
+
+    this.include = (requestType, params) =>
     {
       if(requestType != "GET"){
         this.responseWrapper.console.error(403)
         return
       }
-      let appDir = this.getAppDir()
+      let appDir = this.appDir
       let filePath =  `${appDir}/client/script/apps/${params.app}.json`
-      let prmsLoadFile = new Promise(
-        (resolve, reject) => {
-          fs.readFile(
-            filePath,
-            (err, data) => {
-              if (err){ throw err}
-              let json = JSON.parse(data)
-              resolve(json)
+      let prmsLoadFile = getPrmsLoadFile(filePath)
+      let prmsLoadModules = prmsLoadFile.then(
+        (appJson) => {
+          let services = appJson.services
+          let arrServices = new Array()
+          let prmsLoadServices = services.map(serviceName => {
+              return getPrmsLoadService(appDir, serviceName, arrServices)
             }
           )
-        }
-      )
-      let appJson = prmsLoadFile()
-      let services = appJson.services
-      let prmsLoadServices = services.map(
-        serviceName => {
-          return new Promise(
-            (resolve, reject) =>
-            {
-              let filePathService = `${appDir}/client/script/services/${serviceName}.js`
-              fs.readFile(
-                filePathService,
-                (err, data) => {
-                  if (err){ throw err}
-                  resolve(data)
+
+          let components = appJson.components
+          let arrObjComponents = new Array()
+          let prmsLoadComponents = components.map(componentName => {
+              let dirPathComponent = `${appDir}/client/script/components/${componentName}`
+              return getPrmsLoadComponents(dirPathComponent, componentName, arrObjComponents)
+            }
+          )
+          let prmsLoaders = prmsLoadServices.concat(prmsLoadComponents)
+          return Promise.all(prmsLoaders).then(() => {
+                return new Promise((resolve, reject) => {
+                  resolve({appJson, arrServices, arrObjComponents})
                 }
               )
             }
           )
         }
       )
-      let arrServices = new Array()
-      Promise.all(prmsLoadServices).then(data => arrServices.push(data))
-
-      let components = appJson.components
-      let prmsLoadComponents = components.map(
-        componentName => {
-          let dirPathComponent = `${appDir}/client/script/components/${componentName}`
-          return new Promise(
-            (resolve, reject) => {
-              let objComponent = {name:componentName}
-              let prmsLoadBindings = new Promise((resolve, reject) => {
-                let filePathComponentBindings = `${dirPathComponent}/${componentName}Bindings.json`
-                fs.readFile(
-                  filePathComponentBindings,
-                  (err, data) => {
-                    if (err){ throw err}
-                    objComponent.bindings = data
-                    resolve()
-
-                  }
-                )
-              })
-              let prmsLoadController = new Promise((resolve, reject) => {
-                let filePathComponentController = `${dirPathComponent}/${componentName}Controller.js`
-                fs.readFile(
-                  filePathComponentController,
-                  (err, data) => {
-                    if (err){ throw err}
-                    objComponent.controller = data
-                    resolve()
-                  }
-                )
-              })
-              let prmsLoadTemplate = new Promise((resolve, reject) => {
-                let filePathComponentTemplate = `${dirPathComponent}/${componentName}Template.html`
-                fs.readFile(
-                  filePathComponentTemplate,
-                  (err, data) => {
-                    if (err){ throw err}
-                    objComponent.template = data
-                    resolve()
-                  }
-                )
-              })
-              Promise.all([prmsLoadBindings, prmsLoadController, prmsLoadTemplate])
-              .then(
-                () => {
-                  resolve(objComponent)
-                }
-              )
+      prmsLoadModules.then(
+        (obj) => {
+          let moduleName = `${params.app}Module`
+          let strApp = `var ${moduleName} = angular.module('${params.app}',[]);`
+          obj.appJson.services.forEach(
+            serviceName => {
+              let serviceFunctionName = serviceName.charAt(0).toUpperCase() + params.app.slice(1)
+              strApp += `\r\n${moduleName}.factory('${serviceName}', ${serviceFunctionName}Service);`
             }
           )
+          obj.arrObjComponents.forEach(
+            objComponent => {
+              strApp += '\r\n' + getComponentDeclaration(moduleName, objComponent)
+            }
+          )
+          obj.arrServices.forEach(service => strApp += `\r\n${service}`)
+          obj.arrObjComponents.forEach(objComponent => strApp += `\r\n${objComponent.controller}`)
+          //might make sense to cache strApp somewhere...
+          this.responseWrapper.dynamic(strApp, "js")
         }
       )
-      let arrObjComponents = new Array()
-      Promise.all(prmsLoadComponents).then(obj => arrObjComponents.push(obj))
-      let moduleName = `${params.app}Module`
-      let strApp = `var ${moduleName} = angular.module('${params.app}',[]);`
-      services.forEach(
-        serviceName =>
-          let serviceFunctionName = serviceName.charAt(0).toUpperCase() + params.app.slice(1)
-          strApp += `\r\n${moduleName}.factory('${serviceName}', ${serviceFunctionName}Service);`
+    }
+  }
+}
+function getPrmsLoadFile(filePath){
+  return new Promise(
+    (resolve, reject) => {
+      mdlFs.readFile(
+        filePath,
+        "utf-8",
+        (err, data) => {
+          if (err){ throw err}
+          let json = JSON.parse(data)
+          resolve(json)
+        }
       )
-      arrObjComponents.forEach(
-        objComponent => {
-          let componentName = objComponent.name.replace(/([A-Z])/g, '-$1')
-          let componentFunctionName = componentName.charAt(0).toUpperCase() + params.app.slice(1)
-          strApp += `\r\n${moduleName}.component('${kebabName}',{
+    }
+  )
+}
+function getPrmsLoadService(appDir,serviceName, arrServices){
+  return new Promise(
+    (resolve, reject) => {
+      let filePathService = `${appDir}/client/script/services/${serviceName}.js`
+      mdlFs.readFile(
+        filePathService,
+        "utf-8",
+        (err, data) => {
+          if (err){ throw err}
+          arrServices.push(data)
+          resolve()
+        }
+      )
+    }
+  )
+}
+function getPrmsLoadComponentBindings(dirPathComponent, objComponent){
+  return new Promise(
+    (resolve, reject) => {
+      let filePathComponentBindings = `${dirPathComponent}/${objComponent.name}Bindings.json`
+      mdlFs.readFile(
+        filePathComponentBindings,
+        "utf-8",
+        (err, data) => {
+          if (err){ throw err}
+          objComponent.bindings = data
+          resolve()
+        }
+      )
+    }
+  )
+}
+function getPrmsLoadComponentController(dirPathComponent, objComponent){
+  return new Promise(
+    (resolve, reject) => {
+      let filePathComponentController = `${dirPathComponent}/${objComponent.name}Controller.js`
+      mdlFs.readFile(
+        filePathComponentController,
+        "utf-8",
+        (err, data) => {
+          if (err){ throw err}
+          objComponent.controller = data
+          resolve()
+        }
+      )
+    }
+  )
+}
+function getPrmsLoadComponentTemplate(dirPathComponent, objComponent){
+  new Promise(
+    (resolve, reject) => {
+      let filePathComponentTemplate = `${dirPathComponent}/${objComponent.name}Template.html`
+      mdlFs.readFile(
+        filePathComponentTemplate,
+        "utf-8",
+        (err, data) => {
+          if (err){ throw err}
+          objComponent.template = data
+          resolve()
+        }
+      )
+    }
+  )
+}
+function getPrmsLoadComponents(dirPathComponent, componentName, arrObjComponents){
+  return new Promise(
+    (resolve, reject) => {
+      let objComponent = {name:componentName}
+      arrObjComponents.push(objComponent)
+      let prmsLoadComponentBindings = getPrmsLoadComponentBindings(dirPathComponent, objComponent)
+      let prmsLoadComponentController = getPrmsLoadComponentController(dirPathComponent, objComponent)
+      let prmsLoadComponentTemplate = getPrmsLoadComponentTemplate(dirPathComponent, objComponent )
+      Promise.all(
+        [
+          prmsLoadComponentBindings,
+          prmsLoadComponentController,
+          prmsLoadComponentTemplate
+        ]
+      ).then(
+        () => {
+          resolve()
+        }
+      )
+    }
+  )
+}
+function getComponentDeclaration(moduleName, objComponent){
+  let kebabName = objComponent.name.replace(/([A-Z])/g, '-$1')
+  let componentFunctionName = objComponent.name.charAt(0).toUpperCase() + objComponent.name.slice(1)
+  return `${moduleName}.component('${kebabName}',{
   bindings:${objComponent.bindings},
   controller:${componentFunctionName}Controller,
   template: "${objComponent.template.replace("\"","\\\"")}"
 });`
-        }
-      )
-      arrServices.forEach(service => strApp += `\r\n${service}`)
-      arrObjComponents.forEach(objComponent => strApp += `\r\n${objComponent.controller}`)
-      //might make sense to cache this somewhere...
-      this.responseWrapper.dynamic(strApp, "js")
-    }
-  }
 }
